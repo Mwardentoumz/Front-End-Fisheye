18f6f630483201c5fbe22da5719d88a30509a7e8
Mwardentoumz <thomas.thivolet@gmail.com>
2022-09-07 12:41:43 +0200

    ESLINT
    ESLINT

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,7 +1,7 @@
 
 // DOM ELEMENTS
 const form = document.getElementById('contact_modal')
-
+const send = document.getElementById('send_button')
 
 
 
@@ -26,6 +26,28 @@ function closeModal() {
     const modal = document.getElementById("contact_modal");
     form.style.display = "none";
 }
+
+function getConsoleInputs() {
+    var fields = document.getElementsByClassName('saisie')
+    console.log(fields)
+    for (let item of fields){
+       
+            var usertext = item.value
+            console.log(usertext)
+    }
+        var msg = document.getElementsByClassName('saisie_msg')
+        console.log(msg)
+        for (let item of fields){
+            // item.addEventListener('input', () => {
+                var usertext = item.value
+                console.log(usertext)
+    }
+}
+
+send.addEventListener('click', e => {
+    e.preventDefault()
+    getConsoleInputs()
+})
 
 
 
